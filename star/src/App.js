0f11c0ab0b0f417de39a190f9f52767272526ca8
Paul Donovan <paul.m.donovan@outlook.com>
2024-10-02 10:53:34 +1000

    added projects

---
+++ star/src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import StarRating from './star';
+
+function App() {
+  <StarRating noOfStars={10} />
+}
+
+export default App;
---
+++ star/src/star.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import { FaStar } from 'react-icons/fa';
+import 'src/App.css';
+
+
+export default function StarRating({ noOfStars = 5 }) {
+
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
+
+    function handleClick(getCurrentIndex) {
+        setRating(getCurrentIndex)
+    }
+
+    function handleMouseEnter(getCurrentIndex) {
+        setHover(getCurrentIndex)
+    }
+
+    function handleMouseLeave() {
+        setHover(rating)
+    }
+
+
+    return <div className="">
+        {
+            // array constructor 
+            [...Array(noOfStars)].map((_, index) => {
+                index += 1
+                return <FaStar
+                    key={index}
+                    className={index <= (hover || rating) ? 'active' : 'inactive'}
+                    onClick={() => handleClick(index)}
+                    onMouseMove={() => handleMouseEnter(index)}
+                    onMouseLeave={() => handleMouseLeave()}
+                    size={40}
+                />
+            }
+            )
+        }
+
+
+    </div>
+}
