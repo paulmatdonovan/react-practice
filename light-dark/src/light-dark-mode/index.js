8f20f86ae85fad8a572a5c513d9c6e9fdd3760a9
Paul Donovan <paul.m.donovan@outlook.com>
2024-10-25 20:16:22 +1100

    added a project

---
+++ light-dark/src/light-dark-mode/index.js
@@ -0,0 +1,13 @@
+
+
+
+export default function LightDarkMode() {
+
+
+    return <div>
+        <div className="container">
+            <p>Hello World!</p>
+            <button>Change Theme</button>
+        </div>
+    </div>
+}
