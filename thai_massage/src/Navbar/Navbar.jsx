e2e4a44178905ba5f85d7eb93a6ed08f408b4381
Paul Donovan <paul.m.donovan@outlook.com>
2024-11-30 10:02:51 +1100

    added logo

--- thai_massage/src/Navbar/Navbar.jsx
+++ thai_massage/src/Navbar/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../Images/Nhill Thai massage logo-02.jpg'
 
 const Navbar = () => {
     return (
         <div className='navbar'>
-            <div className='logo'>Nhill Thai Massage</div>
+
+
+            <img className='logo' src={logo} alt="Nhill Thai Massage Logo Healing Lamp" />
+            <div className='store-name'>
+                <h1> Nhill Thai Massage</h1>
+            </div>
             <ul>
                 <li>Home</li>
                 <li>About</li>
