f4b305367489102ba7e256389deca68eeaf89441
Paul Donovan <paul.m.donovan@outlook.com>
2024-12-07 09:25:16 +1100

    changed Servies page

--- thai_massage/src/components/Services.jsx
+++ thai_massage/src/components/Services.jsx
@@ -1,33 +1,39 @@
 import React from 'react'
 import Footer from './Footer'
+import WaterLily from '../Images/water-lily-with-reflection.jpg'
 
 const Services = () => {
     return (
-        <div className='services-page'>                <h1>Price List</h1>
+        <div>
 
             <div className="services">
+                <div className='services-left'>
+                    <h1>Price List</h1>
 
-                {/* use grid-area for four properties 
+                    {/* use grid-area for four properties 
  */}
-                <div className="container">
-
-                    <div className="item1">30 mins</div>
-                    <div className="item2">45 mins</div>
-                    <div className="item3">60 mins</div>
-                    <div className="item4">90 mins</div>
-                    <div className="item5">120 mins</div>
-                    <div className="price1">$60</div>
-                    <div className="price2">$80</div>
-                    <div className="price3">$90</div>
-                    <div className="price4">$100</div>
-                    <div className="price5">$180</div>
-
-
+                    <div className="container">
+                        <div className="item1">30 mins</div>
+                        <div className="item2">45 mins</div>
+                        <div className="item3">60 mins</div>
+                        <div className="item4">90 mins</div>
+                        <div className="item5">120 mins</div>
+                        <div className="price1">$60</div>
+                        <div className="price2">$80</div>
+                        <div className="price3">$90</div>
+                        <div className="price4">$100</div>
+                        <div className="price5">$180</div>
+                    </div>
                 </div>
-
+                <div className='services-right'>
+                    <img src={WaterLily} alt="" />
+                </div>
             </div>
-            <Footer />
+            <div>
+                <Footer />
+            </div>
         </div>
+
     )
 }
 
